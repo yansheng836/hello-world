db2896a26bab3b2814f8d04363949587d3525cad
yansheng836 <1812871846@qq.com>
2019-02-22 21:25:09 +0800

    The first

---
+++ hello world.cpp
@@ -0,0 +1,27 @@
+/*********************************************************************************
+  *Copyright(C),2016-2020,Fosu
+  *FileName: hello world.cpp
+  *Author:	yansheng
+  *Version: 1.0
+  *Date:	2019.2.29
+  *Description:
+  *Function List:
+     1.¡­¡­¡­¡­
+     2.¡­¡­¡­¡­
+  *History:
+     1.Date:
+       Author:
+       Modification:
+     2.¡­¡­¡­¡­
+**********************************************************************************/
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello world!");
+
+
+	return 0;
+}
+
+
